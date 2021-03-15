6254f239df0d4ee983c150482cf72b3ea894606d
Li Zhenhuan <lizhenhuan1019@qq.com>
2021-03-16 00:02:18 +0800

    Update mod version and game version

---
+++ source/CinematicCamera/src/Config/HotKey/CinematicCameraHotkeyCategory.cs
@@ -0,0 +1,42 @@
+﻿using MissionLibrary.HotKey;
+using MissionSharedLibrary.HotKey.Category;
+using System;
+using TaleWorlds.InputSystem;
+
+namespace CinematicCamera.Config.HotKey
+{
+    public enum GameKeyEnum
+    {
+        TogglePlayerInvulnerable,
+        ToggleMapLongPress,
+        NumberOfGameKeyEnums
+    }
+    public class CinematicCameraGameKeyCategory
+    {
+        public const string CategoryId = "CinematicCameraHotKey";
+
+        public static AGameKeyCategory Category => AGameKeyCategoryManager.Get().GetCategory(CategoryId);
+
+        public static void RegisterGameKeyCategory()
+        {
+            AGameKeyCategoryManager.Get()?.AddCategory(CreateCategory, new Version(1, 0));
+        }
+
+        // not enabled
+        public static GameKeyCategory CreateCategory()
+        {
+            var result = new GameKeyCategory(CategoryId,
+                (int)GameKeyEnum.NumberOfGameKeyEnums, GameKeyConfig.Get());
+            result.AddGameKey(new GameKey((int)GameKeyEnum.TogglePlayerInvulnerable, nameof(GameKeyEnum.TogglePlayerInvulnerable),
+                CategoryId, InputKey.M, CategoryId));
+            result.AddGameKey(new GameKey((int)GameKeyEnum.ToggleMapLongPress, nameof(GameKeyEnum.ToggleMapLongPress),
+                CategoryId, InputKey.LeftAlt, CategoryId));
+            return result;
+        }
+
+        public static InputKey GetKey(GameKeyEnum key)
+        {
+            return Category?.GetKey((int)key) ?? InputKey.Invalid;
+        }
+    }
+}
